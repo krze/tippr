9d4844cd7df9fbfbc6a3385a6926532484d59ab7
Ken Krzeminski <krze@tumblr.com>
2015-08-22 15:09:38 -0400

    Ok, restarting the computer fixed my event linking issues. App is now working at bare minimum spec :)

--- tippr/ViewController.swift
+++ tippr/ViewController.swift
@@ -15,9 +15,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
-        // Dragging the Value Changed event to onEditingChanged() did NOT work for me as in the video. It would not allow me to link the event.
-        // I had to do this programmatically
-//        self.tipControl.addTarget(self, action: Selector("onEditingChanged:"), forControlEvents: UIControlEvents.ValueChanged)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
