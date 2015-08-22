ca1f3ad0c8cf702a5abd4643b3ce2ba48483c910
Ken Krzeminski <krze@tumblr.com>
2015-08-22 12:50:35 -0400

    Initial commit

---
+++ tippr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippr
+//
+//  Created by iKreb Retina on 8/22/15.
+//  Copyright (c) 2015 krze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
